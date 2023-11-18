a399cb19f998f17bdd007b1336855bb4a858345b
iagolpriv <iagolpriv@gmail.com>
2023-11-18 23:29:28 +0100

    añadido splash & rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Splash from './Pages/Onboarding/Splash/Splash';
 import Onboarding from './Pages/Onboarding/Onboarding';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="container">
-       <Onboarding />
-      {/* <header className="App-header">
-      </header> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
     </div>
   );
 }

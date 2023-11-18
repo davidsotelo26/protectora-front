dce81374d93a9a1abea2b7509e45acf277ab4801
davidsotelo26 <david.sotelo.rodriguez@bootcamp-upgrade.com>
2023-11-18 22:08:09 +0100

    first version loginPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 import Onboarding from './Pages/Onboarding/Onboarding';
 import LoginCover from './Pages/Login/LoginCover';
+import LoginRegister from './Pages/Login/LoginRegister';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Onboarding />} />
             <Route path="/Login" element={<LoginCover />} />
+            <Route path="/LoginRegister" element={<LoginRegister />} />
           </Routes>
         </Router>
       </body>

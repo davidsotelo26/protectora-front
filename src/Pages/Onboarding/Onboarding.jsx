3d2dce8dd4133da3d3fbf515795dc9becd5b0d35
iagolpriv <iagolpriv@gmail.com>
2023-11-18 19:25:30 +0100

    finalizado onboarding

--- src/Pages/Onboarding/Onboarding.jsx
+++ src/Pages/Onboarding/Onboarding.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import "../Onboarding/Onboarding.scss";
+import image1 from "../../Assets/undrawGoodDoggy4Wfq.png";
+import image2 from "../../Assets/imagen2.png";
+import image3 from "../../Assets/undrawPetAdoption2Qkw.png";
+import cerrarOnboarding from "../../Assets/cerrar.png";
 
 function App() {
   const [activeDot, setActiveDot] = useState(0);
 
   const slides = [
     {
-      image: "path-to-your-image-0.jpg",
+      image: image1,
       text: "Encuentra todo tipo de servicios que tienes cerca de ti",
     },
     {
-      image: "path-to-your-image-1.jpg",
+      image: image2,
       text: "Adopta desde tu móvil",
       text2:
         "Puedes acceder al perfil de muchos animales que están en adopción y filtrarlos para encontrar el que mejor se adapta a ti",
     },
     {
-      image: "path-to-your-image-2.jpg",
+      image: image3,
       text: "Si eres una asociación sube a tus peludos para darles más difusión",
     },
   ];
@@ -27,7 +31,9 @@ function App() {
 
   return (
     <div className="containerOnboarding">
-      <button className="close-btn">X</button>
+      <button className="close-btn">
+        <img src={cerrarOnboarding} alt="Cerrar" />
+      </button>
       <div className="slider">
         <img
           src={slides[activeDot].image}
@@ -35,8 +41,8 @@ function App() {
           className="slider-img"
         />
         <h4 className="slider-title">{slides[activeDot].text}</h4>
+        <p>{slides[activeDot].text2}</p>
         <div className="slider-dots">
-          <p>{slides[activeDot].text2}</p>
           <span
             className={`dot ${activeDot === 0 ? "active" : ""}`}
             onClick={() => handleDotClick(0)}

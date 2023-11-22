d940932b1a8e170100bb3165a3c25081273a6232
Riky Esteban <145079964+destgar@users.noreply.github.com>
2023-11-22 14:23:44 +0100

    Animal detail page (Page: petProfile, Components: AnimalDetailInfo) finished. Faltan estilos

---
+++ src/Components/AnimalDetailInfo/AnimalDetailInfo.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import "./AnimalDetailComponents.scss";
+
+function AnimalDetailInfo() {
+    const { id } = useParams();
+    const [animal, setAnimal] = useState(null); 
+
+    useEffect(() => {
+        const getAnimal = async () => {
+            try {
+                const animalResponse = await axios.get(`http://localhost:3000/animals/id/${id}`);
+                setAnimal(animalResponse.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getAnimal();
+    }, [id]);
+
+    if (!animal) {
+        return <div>Cargando...</div>;
+    }
+
+    return (
+        <div className='animal_detail_component_container'>
+            <div className='animal_detail_line'> <h2>Especie</h2> <p>{animal.species}</p> </div>
+            <div className='animal_detail_line'> <h2>Fecha de nacimiento</h2> <p>{animal.birthDate}</p> </div>
+            <div className='animal_detail_line'> <h2>Sexo</h2> <p>{animal.genre}</p> </div>
+            <div className='animal_detail_line'> <h2>Tamaño</h2> <p>{animal.size}</p> </div>
+            <div className='animal_detail_line'> <h2>Peso</h2> <p>{animal.weight}</p> </div>
+            
+            <div> <h2 className='epigraph_title'>Personalidad</h2> </div>
+            <div> <p>{animal.personality}</p> </div>
+            
+            <div className='epigraph'>
+                <h2 className='epigraph_title'>Historia</h2> 
+                <p className='epigraph_text'>{animal.history}</p>
+            </div>
+        </div>
+    )
+}
+
+export default AnimalDetailInfo;
